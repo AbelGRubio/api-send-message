e25b289af8f28afee7a75677b4bbcebc26d7c653
abel.garcia <ablg11673@gmail.com>
2025-01-07 11:39:04 +0100

    fix(abel): update cors

--- src/app/define_api.py
+++ src/app/define_api.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-# from fastapi.middleware.cors import CORSMiddleware
+from fastapi.middleware.cors import CORSMiddleware
 
 from .configuration import __version__, CORS_ORIGINS
 
@@ -16,11 +16,11 @@ APP.include_router(
     tags=["Service 1: API endpoints"]
 )
 
-# APP.add_middleware(
-#     CORSMiddleware,
-#     allow_origins=CORS_ORIGINS,
-#     allow_credentials=False,
-#     allow_methods=["*"],
-#     allow_headers=["*"],
-# )
+APP.add_middleware(
+    CORSMiddleware,
+    allow_origins=CORS_ORIGINS,
+    allow_credentials=False,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
