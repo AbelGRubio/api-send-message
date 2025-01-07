ba74abedd0964eb130f2e50fee6eb07ba34fe3d9
abel.garcia <ablg11673@gmail.com>
2025-01-07 11:20:24 +0100

    fix(abel): update cors

--- src/app/define_api.py
+++ src/app/define_api.py
@@ -19,7 +19,7 @@ APP.include_router(
 APP.add_middleware(
     CORSMiddleware,
     allow_origins=CORS_ORIGINS,
-    allow_credentials=True,
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
